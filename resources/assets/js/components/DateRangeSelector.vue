290dbdd79dd33cbe84db31247b5993ecae094e7b
lmagiera <lmagiera@gmail.com>
2018-04-03 21:27:33 +0200

    CFA-21 User can navigate date ranges using next and previous buttons

--- resources/assets/js/components/DateRangeSelector.vue
+++ resources/assets/js/components/DateRangeSelector.vue
@@ -5,6 +5,15 @@
             <label class="mr-2">Select Date Range</label>
 
             <div class="input-group input-daterange">
+
+                <div class="input-group-prepend">
+                    <button type="button" class="btn btn-outline-secondary"
+                            v-on:click="prev"
+                            dusk="btn-date-range-prev-control">
+                        <i class="fa fa-lg fa-arrow-circle-left" aria-hidden="true"></i>
+                    </button>
+                </div>
+
                 <input name="from" id="input-date-from-control" type="text" class="form-control" v-model="from" dusk="input-date-from-control">
                 <div class="input-group-append">
                     <div class="input-group-text">
@@ -16,6 +25,13 @@
                     <div class="input-group-text">
                         <i class="fa fa-calendar" aria-hidden="true"></i>
                     </div>
+                </div>
+                <div class="input-group-append">
+                    <button type="button" class="btn btn-outline-secondary"
+                            v-on:click="next"
+                            dusk="btn-date-range-next-control">
+                        <i class="fa fa-lg fa-arrow-circle-right" aria-hidden="true"></i>
+                    </button>
                 </div>
             </div>
 
@@ -44,7 +60,7 @@
 
         mounted() {
 
-            var $this = this;
+            let $this = this;
 
             $('.input-daterange')
                 .datepicker({format: "yyyy-mm-dd", autoclose: true})
@@ -72,9 +88,72 @@
 
             applyDateRange: function() {
 
+                $('.input-daterange input[name="from"]').datepicker('update', this.from);
+                $('.input-daterange input[name="to"]').datepicker('update', this.to);
+
+
                 this.$bus.$emit('date-range-applied', {from: this.from, to: this.to});
 
+            },
+
+            diff: function (from, to) {
+
+                let differences = ['years', 'months', 'days'];
+
+                let unit = 'days';
+                let diff = 1;
+
+                while (differences.length > 0) {
+
+                    unit = differences.shift();
+                    diff = to.diff(from, unit);
+
+                    if ( diff > 0) {
+                        break;
+                    }
+
+                }
+
+                return {diff: diff, unit: unit}
+
+            },
+
+            next: function () {
+
+                let mFrom = moment(this.from);
+                let mTo = moment(this.to).add(1, 'days');
+
+                let {diff, unit} = this.diff(mFrom, mTo);
+
+
+                this.from = mFrom.add(diff, unit).format('YYYY-MM-DD');
+                this.to = mTo.add(diff, unit).subtract(1, 'days').format('YYYY-MM-DD');
+
+                this.applyDateRange();
+
+
+
+            },
+
+            prev: function () {
+
+                let mFrom = moment(this.from);
+                let mTo  = moment(this.to).add(1, 'days');;
+
+                let {diff, unit} = this.diff(mFrom, mTo);
+
+                this.from = mFrom.subtract(diff, unit).format('YYYY-MM-DD');
+                this.to = mTo.subtract(diff, unit).subtract(1, 'days').format('YYYY-MM-DD');
+
+                this.applyDateRange();
+
+
+
+
+
+
             }
+
         }
 
 
