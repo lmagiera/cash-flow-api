b0fba5185c18825ba0a14cd0c462e0779e3c4317
lmagiera <lmagiera@gmail.com>
2018-04-17 00:32:55 +0200

    Codacy Changes

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -12,7 +12,9 @@ try {
     window.$ = window.jQuery = require("jquery");
 
     require("bootstrap");
-} catch (e) {}
+} catch (e) {
+    throw e;
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -30,7 +32,7 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
  * a simple convenience so we don't have to attach every token manually.
  */
 
-let token = document.head.querySelector("meta[name="csrf-token"]");
+let token = document.head.querySelector("meta[name='csrf-token']");
 
 if (token) {
     window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
